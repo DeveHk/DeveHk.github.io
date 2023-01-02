e0835a7989b9ad89b4b7a2ca4d4eb7fa6bc0c1e3
HARSH KUMAR <harshkr202002@outlook.com>
2023-01-02 14:58:42 +0530

    project added

--- src/assets/Components/header/Resume.jsx
+++ src/assets/Components/header/Resume.jsx
@@ -15,8 +15,8 @@ const Resume = () => {
     )
     return (
         <div className="resumeBox-container absolute bottom-10 right-[30%] lg:bottom-[40%] opacity-0 " ref={te}>
-            <div className="resumeBox h-[50px] w-[50px] md:h-[800px] md:w-[800px] absolute -left-20 bottom-10 md:bottom-0 md:-left-28 rotate-45"></div>
-            <div className="resumeBox h-[150px] w-[150px] md:h-[180px] md:w-[180px] flex items-center justify-center   my-10 ">
+            <div className="resumeBox h-[50px] w-[50px] md:h-[800px] md:w-[800px] rounded-3xl absolute -left-20 bottom-10 md:bottom-0 md:-left-28 rotate-45"></div>
+            <div className="resumeBox h-[150px] w-[150px] md:h-[180px] rounded-3xl md:w-[180px] flex items-center justify-center   my-10 ">
                 <a href='src/assets/Harsh_Kumar.pdf' className="resumeLink text-[1.8rem] font-semibold text-[#F9EFF1]">Get Resume</a></div>
         </div>
     )
